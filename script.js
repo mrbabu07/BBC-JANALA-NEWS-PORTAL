f8234a74922cc305226c8f75d987e362f19b4533
md jahedul islam <mdjahedulislamjaved@gmail.com>
2025-09-06 21:38:11 +0600

    added the more api call and functionallity

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 const categoryContainer = document.getElementById("categoryContainer");
 const newsContainer = document.getElementById("newsContainer");
+const bookmarkContainer = document.getElementById("bookmarkContainer")
+let bookmarks = []
 
 const loadCategory = () => {
   fetch("https://news-api-fs.vercel.app/api/categories") //promise
@@ -50,16 +52,45 @@ const showNewsByCategory = (articles) => {
   // clear previous news
   newsContainer.innerHTML = "";
 
-  articles.forEach((article) => {
+  articles.forEach((article, index) => {
     newsContainer.innerHTML += `
-      <div class="border p-3 mb-3">
-        <img src="${article.image?.srcset?.[5]?.url || ''}" alt="news image" class="w-full h-48 object-cover mb-2" />
-        <h1 class="text-lg font-semibold">${article.title}</h1>
-        <p>${article.time}</p>
+      <div class="border rounded-lg p-3 mb-3">
+        <div>
+          <img src="${article.image?.srcset?.[5]?.url || ''}" 
+               alt="news image" 
+               class="w-full h-48 object-cover mb-2" />
+        </div>
+        <div id="${article.id}" class="p-2">
+          <h1 class="text-lg font-extrabold">${article.title}</h1>
+          <p class="text-sm">${article.time}</p>
+          
+          <button class="btn text-sm mt-5">Bookmark</button>
+        </div>
       </div>
     `;
   });
 };
 
+newsContainer.addEventListener("click", (e) =>{
+  // console.log(e.target)
+  //  console.log(e.target.innerText)
+  if(e.target.innerText === 'Bookmark'){
+    handleBookmarks(e)
+  }
+    
+})
+
+const handleBookmarks = (e) => {
+  const title = e.target.parentNode.children[0].innerText
+    const id = e.target.parentNode.id
+    
+    bookmarks.push({
+      title: title,
+      id: id
+    })
+    console.log(bookmarks)
+  }
+
 
 loadCategory();
+loadNewsByCategory("main")
